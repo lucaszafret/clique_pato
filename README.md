9da5791cbc58d27cc9f2309cd0a14e2fdf09174c
Lucas Zafret <lucaszafret@outlook.com>
2025-07-16 21:18:01 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clique_pato
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/br/com/lucaszafret/clique_pato/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.lucaszafret.clique_pato
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
